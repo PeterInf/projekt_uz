c13fedbf2010b85c2bd4485a649be38d028cf159
Krystian Dziędzioła <krystian.dziedziola@gmail.com>
2016-03-22 22:29:23 +0100

    Made documentation and refactoring of classes from edu.projectuz.importers.planuz.model.calendars package

--- src/main/java/edu/projectuz/importers/planuz/model/calendars/Day.java
+++ src/main/java/edu/projectuz/importers/planuz/model/calendars/Day.java
@@ -1,5 +1,15 @@
 package edu.projectuz.importers.planuz.model.calendars;
 
+/**
+ * This class is the lowest in planUz calendars hierarchy.
+ * Instance of this class represents single day of calendar.
+ * It stores information about day number, date and two different names.
+ * This two names are necessary because sometimes one particular day
+ * of the week there is treated as if it was some other day.
+ * For example: one day there'a a friday but all of the classes are
+ * by a monday schedule.
+ * It's a part of {@link DaysList} class.
+ */
 public class Day {
 
     private int number;
@@ -7,6 +17,13 @@ public class Day {
     private String dayAccordingToTimetable;
     private String getDayAccordingToCalendar;
 
+    /**
+     * Class constructor that simply sets all of the member variables describing day.
+     * @param number - Number of a day.
+     * @param date - Date of a day.
+     * @param dayAccordingToTimetable - Day name according to timetable.
+     * @param getDayAccordingToCalendar - Day name according to calendar.
+     */
     public Day(int number, String date, String dayAccordingToTimetable, String getDayAccordingToCalendar) {
         this.number = number;
         this.date = date;
@@ -14,10 +31,18 @@ public class Day {
         this.getDayAccordingToCalendar = getDayAccordingToCalendar;
     }
 
+    /**
+     * Gets value of date variable.
+     * @return Returns value of date variable.
+     */
     public String getDate() {
         return date;
     }
 
+    /**
+     * Converts object of this class to String object with all of it's content.
+     * @return Returns converted String.
+     */
     @Override
     public String toString() {
         return "Day{" +
