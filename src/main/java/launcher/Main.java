63dc01c1e2f0f74fc5dc23fc5ee2f9a3477ae14e
Bartosz Stępień <bartosz.stepien@outlook.com>
2016-03-22 23:25:33 +0100

    Create test implementation of log4j2

--- src/main/java/launcher/Main.java
+++ src/main/java/launcher/Main.java
@@ -1,14 +1,19 @@
 package launcher;
 
 import org.apache.catalina.startup.Tomcat;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Optional;
 
 public class Main {
 
-    public static final Optional<String> port = Optional.ofNullable(System.getenv("PORT"));
+    private static final Optional<String> port = Optional.ofNullable(System.getenv("PORT"));
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
+        logger.debug("==== START mCAL server ====");
         String contextPath = "";
         String appBase = ".";
         Tomcat tomcat = new Tomcat();
