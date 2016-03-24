cac9bd5590766b014db37f56951c0deac30d65ac
Bartosz Sledz <bartosz.sledz94@gmail.com>
2016-03-24 18:06:50 +0100

    Made a little corrections.

--- src/main/java/edu/projectuz/importers/csv/logic/CSVImporterTest.java
+++ src/main/java/edu/projectuz/importers/csv/logic/CSVImporterTest.java
@@ -12,20 +12,22 @@ import static junit.framework.TestCase.assertEquals;
 
 public class CSVImporterTest {
 
-    private String filePath = "D:/Programy/IntelliJ IDEA/Projekty/webapp/src/main/java/edu/projectuz/importers/csv/resourcestmp/filetest.csv";
     private CSVImporter csvImporter;
 
     @Before
     public void setUp() throws Exception {
+        String filePath = "D:/Programy/IntelliJ IDEA/Projekty/webapp/src/main/java/edu/projectuz/importers/csv/resourcestmp/filetest.csv";
         this.csvImporter = new CSVImporter(filePath);
     }
 
+    @SuppressWarnings("deprecation")
     @Test
     public void testConvertCsvToObjectTrue() throws Exception {
-        assertEquals(new CalendarEvent("TPI KOLO", new Date(Date.parse("2016/01/22 00:00")), new Date(Date.parse("2016/01/23 00:00")), "OPIS", "f", TimeZone.getTimeZone("America/Los_Angeles")),
+        assertEquals(new CalendarEvent("TPI KOŁO", new Date(Date.parse("2016/01/22 00:00")), new Date(Date.parse("2016/01/23 00:00")), "OPIS", "f", TimeZone.getTimeZone("America/Los_Angeles")),
                 csvImporter.convertCsvToObject().get(0));
     }
 
+    @SuppressWarnings("deprecation")
     @Test
     public void testConvertCsvToObjectFalse() throws Exception {
         assertEquals(new CalendarEvent("TPIFALSE", new Date(Date.parse("2016/01/22 00:00")), new Date(Date.parse("2016/01/23 00:00")), "O", "f", TimeZone.getTimeZone("America")),
