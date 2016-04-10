2c00855022477ab3e10af7878c68e48b668ed336
Kamil Kiliszek <kamilo7654@gmail.com>
2016-04-10 15:15:27 +0200

    Add logger and helperForParser method

--- src/main/java/edu/projectuz/mCal/importers/xml/XMLImporter.java
+++ src/main/java/edu/projectuz/mCal/importers/xml/XMLImporter.java
@@ -4,6 +4,8 @@ import edu.projectuz.mCal.helpers.DateHelper;
 import edu.projectuz.mCal.core.models.CalendarEvent;
 import edu.projectuz.mCal.importers.base.BaseEventImporter;
 import edu.projectuz.mCal.importers.base.ImporterSourceType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.xml.sax.InputSource;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -17,47 +19,52 @@ import java.io.StringReader;
 
 public class XMLImporter extends BaseEventImporter{
 
+    private final Logger logger = LogManager.getLogger(XMLImporter.class);
+
     public XMLImporter(String sourcePath, ImporterSourceType sourceType) {
         super(sourcePath, sourceType);
     }
 
     public ArrayList<CalendarEvent> convertToObject()
     {
-        ArrayList<CalendarEvent> listOfEvents = new ArrayList<CalendarEvent>();
+        String dateFormat = "yyyy-MM-dd";
+        ArrayList<CalendarEvent> listOfEvents = new ArrayList<>();
 
         try {
 
             DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
             Document document = documentBuilder.parse(new InputSource(new StringReader(getSourceContent())));
-
             NodeList nodeList = document.getElementsByTagName("vevent");
-            String dateFormat = "yyyy-MM-dd";
 
             for (int i=0; i<nodeList.getLength(); i++) {
 
-                CalendarEvent eventObject = new CalendarEvent();
-                Node node = nodeList.item(i);
-
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-
-                    Element element = (Element) node;
-
-                    eventObject.setStartDate(DateHelper.stringToDate(element.getElementsByTagName("dtstart").item(0).getTextContent().trim(),
-                            dateFormat, DateHelper.stringToTimeZone(element.getElementsByTagName("tzid").item(0).getTextContent().trim())));
-                    eventObject.setDescription(element.getElementsByTagName("summary").item(0).getTextContent().trim());
-                    listOfEvents.add(eventObject);
-
-                }
+                helperForParser(nodeList, listOfEvents, dateFormat, i);
             }
 
         } catch (Exception e)
         {
             e.printStackTrace();
+            logger.debug(e.getMessage());
         }
         return listOfEvents;
     }
 
+    private void helperForParser(NodeList nodeList, ArrayList<CalendarEvent> listOfEvents, String dateFormat, int i) {
+
+        CalendarEvent eventObject = new CalendarEvent();
+        Node node = nodeList.item(i);
+
+        if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+            Element element = (Element) node;
+
+            eventObject.setStartDate(DateHelper.stringToDate(element.getElementsByTagName("dtstart").item(0).getTextContent().trim(),
+                    dateFormat, DateHelper.stringToTimeZone(element.getElementsByTagName("tzid").item(0).getTextContent().trim())));
+            eventObject.setDescription(element.getElementsByTagName("summary").item(0).getTextContent().trim());
+            listOfEvents.add(eventObject);
+        }
+    }
 
     @Override
     public String getName() {
