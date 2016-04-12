9a86e85b56879df97e9e384682c93ef9dbcffa56
Sebastian Sobierajski <sivik99@onet.eu>
2016-04-12 14:22:17 +0200

    Refactor namespaces and rebase

---
+++ src/main/java/edu/projectuz/mCal/importers/ical/logic/ICalImporter.java
@@ -0,0 +1,22 @@
+package edu.projectuz.mCal.importers.ical.logic;
+
+import edu.projectuz.mCal.importers.base.BaseEventImporter;
+import edu.projectuz.mCal.importers.base.ImporterSourceType;
+
+
+public class ICalImporter extends BaseEventImporter {
+
+    protected ICalImporter(String sourcePath, ImporterSourceType sourceType) {
+        super(sourcePath, sourceType);
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void importData() {
+
+    }
+}
