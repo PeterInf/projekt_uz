cdfaccad3b39157c29473d3ae61c752c678ce440
Krystian Dziędzioła <krystian.dziedziola@gmail.com>
2016-04-12 17:09:05 +0200

    Started writing tests for edu.projectuz.mCal.importers.planuz.logic.PlanUzImporter

---
+++ src/test/java/edu/projectuz/mCal/importers/planuz/logic/timetables/DepartmentsListImporterTest.java
@@ -0,0 +1,21 @@
+package edu.projectuz.mCal.importers.planuz.logic.timetables;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class DepartmentsListImporterTest {
+
+    private DepartmentsListImporter departmentsListImporter;
+
+    @Before
+    public void setUp() {
+        departmentsListImporter = new DepartmentsListImporter("http://plan.uz.zgora.pl/grupy_lista_kierunkow.php");
+    }
+
+    @Test
+    public void whenDepartmentsImportedThenSizeMatches() {
+        assertEquals(12, departmentsListImporter.importDepartments().size());
+    }
+}
