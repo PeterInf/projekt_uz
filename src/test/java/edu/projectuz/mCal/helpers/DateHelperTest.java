30a082570955d32efe350cc450de47a2861fcff7
Bartosz Sledz <bartosz.sledz94@gmail.com>
2016-04-15 09:27:38 +0200

    Fixed test class for new DateHelper class.

--- src/test/java/edu/projectuz/mCal/helpers/DateHelperTest.java
+++ src/test/java/edu/projectuz/mCal/helpers/DateHelperTest.java
@@ -1,8 +1,15 @@
 package edu.projectuz.mCal.helpers;
 
 import junitparams.JUnitParamsRunner;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.TimeZone;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(JUnitParamsRunner.class)
 public class DateHelperTest {
@@ -10,18 +17,16 @@ public class DateHelperTest {
     @Test
     public void convertStringToDate() throws Exception {
         //Arrange
-        /*String dateText = "04-08-1994 12:44:33";
-        TimeZone timeZone = TimeZone.getTimeZone("Europe/Warsaw");
-        String dateFormat = "dd-mm-yyyy HH:mm:ss";
+        String dateText = "04-08-1994 12:44:33";
+        TimeZone timeZone = TimeZone.getTimeZone("America/Los_Angeles");
+        String dateFormat = "dd-mm-yyyy kk:mm:ss";
 
-        Calendar cal = Calendar.getInstance();
-        cal.set(1994, Calendar.JANUARY, 4, 12, 44, 33); //Year, month, day of month, hours, minutes and seconds
-        Date expected = cal.getTime();
+        DateTime expected = new DateTime(DateTimeFormat.forPattern(dateFormat).withZone(DateTimeZone.forTimeZone(timeZone)).parseDateTime(dateText));
 
         //Act
         DateTime result = DateHelper.stringToDate(dateText, dateFormat, timeZone);
 
         //Assert
-        assertEquals(expected.toString(), result.toString());*/
+        assertEquals(expected.toString(), result.toString());
     }
 }
