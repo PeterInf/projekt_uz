be8e32a4452002fdde379c4c298aa7a999b31f62
Robert Koziołek <robertkoziolek4c@gmail.com>
2016-04-21 23:47:52 +0200

    Zaimplikowanie metody getCalendarEventList w ICalImporterze

--- src/main/java/edu/projectuz/mCal/importers/ical/logic/ICalImporter.java
+++ src/main/java/edu/projectuz/mCal/importers/ical/logic/ICalImporter.java
@@ -1,13 +1,25 @@
 package edu.projectuz.mCal.importers.ical.logic;
 
+import edu.projectuz.mCal.core.models.CalendarEvent;
 import edu.projectuz.mCal.importers.base.BaseEventImporter;
 import edu.projectuz.mCal.importers.base.ImporterSourceType;
 import edu.projectuz.mCal.importers.ical.model.ICalModel;
+import edu.projectuz.mCal.importers.planuz.model.calendars.Calendar;
+import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.data.CalendarParserImpl;
 import net.fortuna.ical4j.data.HCalendarParser;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.Component;
+import net.fortuna.ical4j.model.component.VEvent;
+import net.fortuna.ical4j.model.property.Location;
 
 import javax.annotation.Resource;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ICalImporter extends BaseEventImporter {
@@ -28,8 +40,33 @@ public class ICalImporter extends BaseEventImporter {
     }
 
     public void getContent(){
-        //
+
     }
 
+    public ArrayList<CalendarEvent> getCalendarEventList() {
+        InputStream is = new ByteArrayInputStream(getSourceContent().getBytes());
+        CalendarBuilder builder = new CalendarBuilder();
+        net.fortuna.ical4j.model.Calendar calendar = null;
+        try {
+            calendar = builder.build(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParserException e) {
+            e.printStackTrace();
+        }
+        ArrayList<VEvent> vevents = calendar.getComponents(Component.VEVENT);
+        ArrayList<CalendarEvent> events = new ArrayList<CalendarEvent>();
+        for (VEvent ev : vevents){
+            CalendarEvent event = new CalendarEvent();
+            //event.setStartDate() // tutaj data
+            //event.setEndDate() // tutaj tysz
+            event.setTitle(ev.getName());//not sure
+            event.setDescription(ev.getDescription().getValue());
+            //event.setTimeZone()//;c
+            events.add(event);
+        }
 
+
+        return events;
+    }
 }
