c117d748a84783eef4316a136d5340a194cd7e81
Sebastian Sobierajski <sivik99@onet.eu>
2016-04-25 17:35:42 +0200

    Description class and refactor ICalImporter and remove ex file

--- src/main/java/edu/projectuz/mCal/importers/ical/logic/ICalImporter.java
+++ src/main/java/edu/projectuz/mCal/importers/ical/logic/ICalImporter.java
@@ -4,31 +4,37 @@ import edu.projectuz.mCal.core.models.CalendarEvent;
 import edu.projectuz.mCal.importers.base.BaseEventImporter;
 import edu.projectuz.mCal.importers.base.ImporterSourceType;
 import edu.projectuz.mCal.importers.ical.model.ICalModel;
-import edu.projectuz.mCal.importers.planuz.model.calendars.Calendar;
 import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.data.CalendarParserImpl;
-import net.fortuna.ical4j.data.HCalendarParser;
 import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.Calendar;
 import net.fortuna.ical4j.model.Component;
 import net.fortuna.ical4j.model.component.VEvent;
-import net.fortuna.ical4j.model.property.Location;
-
-import javax.annotation.Resource;
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.List;
 
 
+/**
+ * This class is main class for importer all data from ICal.
+ */
 public class ICalImporter extends BaseEventImporter {
+
+    /**
+     * @param sourcePath specifies the path of the file.
+     * @param sourceType specifies the type of resource {@link ImporterSourceType}.
+     */
 
     protected ICalImporter(String sourcePath, ImporterSourceType sourceType) {
         super(sourcePath, sourceType);
     }
     public ICalModel iCalModel = new ICalModel();
     private CalendarParserImpl Parser = new CalendarParserImpl();
+
+    /**
+     * This method return name importer.
+     */
     @Override
     public String getName() {
         return "ICalImporter";
@@ -39,14 +45,16 @@ public class ICalImporter extends BaseEventImporter {
 
     }
 
-    public void getContent(){
-
-    }
-
-    public ArrayList<CalendarEvent> getCalendarEventList() {
+/**
+ * This is a main function of this class.
+ * It is used to import all data from ICal.
+ *
+ * @return Returned list of events.
+ * */
+    public ArrayList<CalendarEvent> getCalendarEventList() throws Exception {
         InputStream is = new ByteArrayInputStream(getSourceContent().getBytes());
         CalendarBuilder builder = new CalendarBuilder();
-        net.fortuna.ical4j.model.Calendar calendar = null;
+        Calendar calendar = null;
         try {
             calendar = builder.build(is);
         } catch (IOException e) {
@@ -56,17 +64,19 @@ public class ICalImporter extends BaseEventImporter {
         }
         ArrayList<VEvent> vevents = calendar.getComponents(Component.VEVENT);
         ArrayList<CalendarEvent> events = new ArrayList<CalendarEvent>();
-        for (VEvent ev : vevents){
-            CalendarEvent event = new CalendarEvent();
-            //event.setStartDate() // tutaj data
-            //event.setEndDate() // tutaj tysz
-            event.setTitle(ev.getName());//not sure
-            event.setDescription(ev.getDescription().getValue());
-            //event.setTimeZone()//;c
-            events.add(event);
+        try {
+            for (VEvent ev : vevents) {
+                CalendarEvent event = new CalendarEvent();
+                //event.setStartDate() // tutaj data
+                //event.setEndDate() // tutaj tysz
+                event.setTitle(ev.getName());//not sure
+                event.setDescription(ev.getDescription().getValue());
+                //event.setTimeZone()//;c
+                events.add(event);
+            }
+        }catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException();
         }
-
-
         return events;
     }
 }
