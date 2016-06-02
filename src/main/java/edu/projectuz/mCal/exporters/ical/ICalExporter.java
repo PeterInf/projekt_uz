e0be2cc2f0c19a4fd2067cef65f5daf700adabf8
Sebastian Sobierajski <sivik99@onet.eu>
2016-06-02 18:54:22 +0200

    ICalExporter changing in exporter

--- src/main/java/edu/projectuz/mCal/exporters/ical/ICalExporter.java
+++ src/main/java/edu/projectuz/mCal/exporters/ical/ICalExporter.java
@@ -1,6 +1,7 @@
 package edu.projectuz.mCal.exporters.ical;
 
 import edu.projectuz.mCal.core.models.CalendarEvent;
+import edu.projectuz.mCal.helpers.DateHelper;
 import edu.projectuz.mCal.importers.ical.logic.ICalHelper;
 import edu.projectuz.mCal.importers.ical.logic.ICalRegexSections;
 import net.fortuna.ical4j.data.CalendarBuilder;
@@ -18,63 +19,19 @@ public class ICalExporter {
 
     /**
      * This is function of this class.
-     * It is used to create list events from model.
-     */
-
-    public ArrayList<CalendarEvent> GetDataFromModel(){
-        //dostanie sie po obiekty eventow DAO/HIBERNATE
-        //pobierz obiekty eventow z bazy
-        ArrayList<CalendarEvent> vevents = new ArrayList<>();
-        for(CalendarEvent vevent: vevents){
-            vevent.add(obiektDAO);
-        }
-        return vevents;
-    }
-
-    /**
-     * This is function of this class.
-     * It is used to import data from database to ICalCalendar.
-     */
-
-    public ArrayList<CalendarEvent> CreateICalObjectFromDatabase(){
-        CalendarBuilder builder = new CalendarBuilder();
-        Calendar calendar = null;
-        ArrayList<CalendarEvent> events = new ArrayList<>();
-
-        ArrayList<CalendarEvent> vevents = GetDataFromModel();
-        try {
-            for (CalendarEvent ev : vevents) {
-                CalendarEvent event = new CalendarEvent();
-                event.setStartDate(ev.getStartDate());
-                event.setEndDate(ev.getEndDate());
-                event.setTitle(ev.getTitle());
-                event.setDescription(ev.getDescription());
-                event.setTimeZone(ev.getTimeZone());
-                event.setTag("");
-                events.add(event);
-            }
-        }catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException();
-        }
-        return events;
-    }
-
-    /**
-     * This is function of this class.
      * It is used to export data to File.
      */
 
-    public String generateICal(String fileName) throws IOException {
+    public String generateICal(ArrayList<CalendarEvent> listOfEvents) throws IOException {
         StringBuilder stringBuilder = new StringBuilder();
         String tempTime;
         String tempDate;
         String tempDateTime;
-        ArrayList<CalendarEvent> calendarEvents = CreateICalObjectFromDatabase();
         stringBuilder.append(ICalSections.BEGIN);
         stringBuilder.append(ICalSections.VCALENDAR);
         stringBuilder.append('\n');
         //Event Section
-        for (CalendarEvent ev : calendarEvents){
+        for (CalendarEvent ev : listOfEvents){
             stringBuilder.append(ICalSections.BEGIN);
             stringBuilder.append(ICalSections.VEVENT);
             stringBuilder.append('\n');
@@ -82,8 +39,7 @@ public class ICalExporter {
             stringBuilder.append(ICalSections.TIMEZONEID);
             stringBuilder.append(DateHelper.stringToTimeZone(ICalHelper.getGroupFromDate(ev.getEndDate().toString(), ICalRegexSections.TIMEZONE)));
             stringBuilder.append(ICalSections.COLONSEPARATOR);
-            tempDateTime = DateHelper.stringToDate(ICalHelper.getGroupFromDate(ev.getStartDate().toString(), ICalRegexSections.DATE) +
-                    ICalHelper.getGroupFromDate(ev.getStartDate().toString(), ICalRegexSections.TIME), dateFormat);
+            tempDateTime = ev.getStartDate().toString();
             tempDate = tempDateTime.substring(0,9);
             tempTime = tempDateTime.substring(8,tempDateTime.length());
             stringBuilder.append(tempDate);
@@ -91,8 +47,7 @@ public class ICalExporter {
             stringBuilder.append(tempTime);
             stringBuilder.append('\n');
             stringBuilder.append(ICalSections.DATE_END);
-            tempDateTime =DateHelper.stringToDate(ICalHelper.getGroupFromDate(ev.getEndDate().toString(),  ICalRegexSections.DATE) +
-                    ICalHelper.getGroupFromDate(ev.getEndDate().toString(), ICalRegexSections.TIME), dateFormat);
+            tempDateTime =ev.getEndDate().toString();
             tempDate = tempDateTime.substring(0,9);
             tempTime = tempDateTime.substring(8,tempDateTime.length());
             stringBuilder.append(tempDate);
@@ -112,10 +67,8 @@ public class ICalExporter {
             stringBuilder.append(ICalSections.VEVENT);
             stringBuilder.append('\n');
         }
-
         stringBuilder.append(ICalSections.END);
         stringBuilder.append(ICalSections.VCALENDAR);
-
         return stringBuilder.toString();
     }
 }
